0898944a6a148a831b453b1c8edddfd1303db46f
吴凡-去哪 <13161666712@163.com>
2018-06-09 00:49:33 +0800

    2018-6-9

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="header">
-        <h1 class="dell">{{ msg }}</h1>
+        <div class="dell" @click="fn">{{ msg }}</div>
     </div>
 </template>
-
 <script>
 export default {
   name: 'Header',
@@ -11,16 +10,20 @@ export default {
     return {
       msg: 'hello header'
     }
-
+  },
+  methods: {
+    fn () {
+      this.msg = 'hello wrold'
+    }
   }
 }
 </script>
-
 <style scoped>
 .dell {
+    height:50px;
+    line-height:50px;
+    font-size:40px;
     background:#d00;
     color:#dd0;
 }
 </style>
-
-
