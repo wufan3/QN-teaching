0551be9e44b3bbcd425601c9c15b5c2d30ebcb1c
吴凡-去哪 <13161666712@163.com>
2018-06-10 20:39:42 +0800

    2018-6-10 1次

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
     <Header/>
-    <router-view/>
+    <!-- <router-view/> -->
   </div>
 </template>
-
 <script>
 import Header from './components/Header'
-
 export default {
   name: 'App',
-  
   components: {
     Header
   }
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,29 @@
 <template>
     <div class="header">
-        <h1>{{ msg }}</h1>
+        <h1 class="dell" @click="fn">{{ msg }}</h1>
     </div>
 </template>
-
 <script>
 export default {
-    name: 'Header',
-    data () {
-        return {
-            msg: 'hello header'
-        }
-
+  name: 'Header',
+  data () {
+    return {
+      msg: 'hello header'
     }
+  },
+  methods: {
+    fn () {
+      this.msg = 'hello world'
+    }
+  }
 }
 </script>
-
-<style>
-
+<style scoped>
+  .dell{
+    height:100px;
+    background:#dd0;
+    color:#d00;
+    font-size:60px;
+    line-height:100px;
+  }
 </style>
-
-
